ee2ec67cc4cdcd59e10dadae56390eb730d55b27
freejob985 <freejob985@gmail.com>
2020-12-30 00:20:33 +0200

    sss

--- resources/views/frontend/default/Offers.blade.php
+++ resources/views/frontend/default/Offers.blade.php
@@ -140,7 +140,7 @@
                 <p class="be-comment-text">
                     {{ $item_project_bids->message}}
                     <p>
-                        <button type="button" class="btn btn-success btn-sm">{{ $item_project_bids->amount}} العرض المقدم  &nbsp; &nbsp; &nbsp;</button>
+                        <button type="button" class="btn btn-success btn-sm"> العرض المقدم  &nbsp; &nbsp; &nbsp; <span class="label label-default">{{ $item_project_bids->amount}}</span></button>
                     </p>
                 </p>
 
