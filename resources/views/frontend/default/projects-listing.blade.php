32bbaeb57ddad488089f4d218e67bf8199a4110f
freejob985 <freejob985@gmail.com>
2020-12-30 01:11:31 +0200

    سسس

--- resources/views/frontend/default/projects-listing.blade.php
+++ resources/views/frontend/default/projects-listing.blade.php
@@ -55,18 +55,14 @@
                                                     checked
                                                 @endif onchange="applyFilter()"> {{ translate('Fixed Price') }}
                                                 <span class="aiz-square-check"></span>
-                                                <span class="float-right text-secondary fs-12" style="
-                                                display: none;
-                                            " >(200)</span>
+                                                <span class="float-right text-secondary fs-12" >({{ DB::table('projects')->where('type',"Fixed")->count() }})</span>
                                             </label>
                                             <label class="aiz-checkbox">
                                                 <input type="checkbox"  name="projectType[]" value="Long Term" @if (in_array('Long Term', $projectType))
                                                     checked
                                                 @endif onchange="applyFilter()"> {{ translate('Long Term') }}
                                                 <span class="aiz-square-check"></span>
-                                                <span class="float-right text-secondary fs-12" style="
-                                                display: none;
-                                            ">(200)</span>
+                                                <span class="float-right text-secondary fs-12">({{ DB::table('projects')->where('type',"long_term")->count() }})</span>
                                             </label>
                                         </div>
                                     </div>
