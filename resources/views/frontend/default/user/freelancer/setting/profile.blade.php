caf44f1eb40dea5da39643fe5930794408e836f2
freejob985 <freejob985@gmail.com>
2020-12-31 22:11:43 +0200

    شسي

--- resources/views/frontend/default/user/freelancer/setting/profile.blade.php
+++ resources/views/frontend/default/user/freelancer/setting/profile.blade.php
@@ -2,7 +2,7 @@
 @extends('frontend.default.layouts.app')
 
 @section('content')
-<link rel="stylesheet" href="path/to/line-awesome/css/line-awesome-font-awesome.min.css">
+<link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
     <section class="py-5">
         <div class="container">
             <div class="d-flex align-items-start">
