ca087eff445a2a7cf62dd90725d3ba21b317ddc6
freejob985 <freejob985@gmail.com>
2020-12-31 19:22:42 +0200

    asd

--- app/Http/Controllers/SystemConfigurationController.php
+++ app/Http/Controllers/SystemConfigurationController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\SystemConfiguration;
 use Gate;
+use Session;
 
 class SystemConfigurationController extends Controller
 {
