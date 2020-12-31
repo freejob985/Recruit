2a9e5567588cad9871a72f891abf4717657e2ff0
freejob985 <freejob985@gmail.com>
2020-12-31 02:59:39 +0200

    as

--- resources/views/admin/default/layouts/app.blade.php
+++ resources/views/admin/default/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
 	<meta name="csrf-token" content="{{ csrf_token() }}">
 	<meta name="app-url" content="{{ env('APP_URL')}}">
@@ -11,7 +12,7 @@
 	<title>{{ config('app.name', 'Active Workdesk') }}</title>
 
 	<!-- Favicon -->
-    <link rel="icon" href="{{ custom_asset(get_setting('site_icon')) }}">
+	<link rel="icon" href="{{ custom_asset(get_setting('site_icon')) }}">
 
 	<!-- google font -->
 	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700">
@@ -19,22 +20,30 @@
 	<!-- aiz core css -->
 	<link rel="stylesheet" href="{{ my_asset('assets/common/css/vendors.css') }}">
 	<link rel="stylesheet" href="{{ my_asset('assets/common/css/aiz-core.css') }}">
+	<style>
+		@import url('https://fonts.googleapis.com/css2?family=Markazi+Text:wght@600&display=swap');
 
+		* {
+			font-family: 'Markazi Text', serif;
+
+		}
+	</style>
 
 	<script>
-    	var AIZ = AIZ || {};
+		var AIZ = AIZ || {};
 	</script>
 
 </head>
+
 <body>
 
 	<div class="aiz-main-wrapper">
-        @include('admin.default.inc.admin-sidebar')
+		@include('admin.default.inc.admin-sidebar')
 		<div class="aiz-content-wrapper">
-            @include('admin.default.inc.admin-topbar')
+			@include('admin.default.inc.admin-topbar')
 			<div class="aiz-main-content">
 				<div class="px-15px px-lg-25px">
-                    @yield('content')
+					@yield('content')
 				</div>
 				<div class="bg-white text-center py-3 px-15px px-lg-25px mt-auto">
 					<p class="mb-0">&copy; {{ env('APP_NAME') }} v{{ get_setting('current_version') }}</p>
@@ -43,19 +52,20 @@
 		</div><!-- .aiz-content-wrapper -->
 	</div><!-- .aiz-main-wrapper -->
 
-    @yield('modal')
+	@yield('modal')
 
 
-	<script src="{{ my_asset('assets/common/js/vendors.js') }}" ></script>
-	<script src="{{ my_asset('assets/common/js/aiz-core.js') }}" ></script>
+	<script src="{{ my_asset('assets/common/js/vendors.js') }}"></script>
+	<script src="{{ my_asset('assets/common/js/aiz-core.js') }}"></script>
 
-    @yield('script')
+	@yield('script')
 
-    <script type="text/javascript">
-    @foreach (session('flash_notification', collect())->toArray() as $message)
+	<script type="text/javascript">
+		@foreach (session('flash_notification', collect())->toArray() as $message)
         AIZ.plugins.notify('{{ $message['level'] }}', '{{ $message['message'] }}');
     @endforeach
-    </script>
+	</script>
 
 </body>
+
 </html>
