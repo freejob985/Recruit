080239b657c401f757c2345ab91c6fa716385ec0
freejob985 <freejob985@gmail.com>
2020-12-31 23:23:04 +0200

    شسي

---
+++ app/Http/Middleware/Maintenance.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use App;
+use Session;
+use Config;
+
+class Maintenance
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $Status = DB::table('Maintenance')->value('Status');
+
+        if($Status=="Status"){
+            dd("Catch errors for script and full tracking ( 1 )");
+        }
+        else{
+           // dd("Catch errors for script and full tracking ( 2 )");
+            $locale = 'sa';
+        }
+
+       
+        return $next($request);
+    }
+}
