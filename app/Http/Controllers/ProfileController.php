686e498886eecb47a8b37553f8cf37e737fecca1
freejob985 <freejob985@gmail.com>
2020-12-31 22:50:07 +0200

    asd

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -79,10 +79,11 @@ class ProfileController extends Controller
         }
         $user->name = $request->name;
         if ($user->save()) {
+            dd($request->specialist);
             $user_profile = UserProfile::where('user_id', Auth::user()->id)->where('user_role_id', Session::get('role_id'))->first();
             $user_profile->gender = $request->gender;
             $user_profile->nationality = $request->nationality;
-            $user_profile->specialist = implode(",",$request->specialist[]);
+            $user_profile->specialist = json_encode($request->specialist[]);
             if ($request->hourly_rate != null) {
                 $user_profile->hourly_rate = $request->hourly_rate;
             }
