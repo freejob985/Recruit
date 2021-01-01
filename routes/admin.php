42b8aaed4563bdf611422e0a64c32d4eaabe1adc
freejob985 <freejob985@gmail.com>
2021-01-01 08:57:46 +0200

    asd

--- routes/admin.php
+++ routes/admin.php
@@ -31,6 +31,8 @@ Route::group(['prefix' => 'admin', 'middleware' => ['auth', 'admin']], function 
     Route::get('/client-package-index/{type}', 'PackageController@index')->name('client_package.index');
     Route::get('/client-package-create/{type}', 'PackageController@create')->name('client_package.create');
     Route::get('/client-package-edit/{id}', 'PackageController@edit')->name('client_package.edit');
+    Route::get('/client-package-edit/{id}', 'PackageController@edit')->name('comprehensive_package.edit');
+
 
     //LanguageController for Freelancer and Client
     Route::resource('/languages', 'LanguageController');
