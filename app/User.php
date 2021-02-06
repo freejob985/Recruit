ddb3db69f1119f5560ab227e7b5caa59d9443015
freejob985 <freejob985@gmail.com>
2021-02-07 00:45:15 +0200

    asd

--- app/User.php
+++ app/User.php
@@ -23,6 +23,8 @@ use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
+
+
 class User extends Authenticatable
 {
     use Notifiable;
